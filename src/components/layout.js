0ff0b7bed4573d9a8426362558f4d02fbf7fbf88
Samuel Kim <samuelkim@Samuels-MacBook-Pro.local>
2019-11-03 16:42:23 -0800

    created sidebar and footer components

--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from "./Footer"
 import "../styles/index.scss"
 
 const Layout = ({ children }) => {
@@ -25,15 +26,15 @@ const Layout = ({ children }) => {
 
   return (
     <>
+      <script
+        src="https://kit.fontawesome.com/29565e8ac4.js"
+        crossOrigin="anonymous"
+      ></script>
       <Header siteTitle={data.site.siteMetadata.title} />
       <div className="container" id="content">
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
       </div>
+      <Footer />
     </>
   )
 }
